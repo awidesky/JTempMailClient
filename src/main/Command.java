78da97fb93c9e0c4c58442ba08f22d0fec730d01
awidesky <awidesky@naver.com>
2022-09-26 20:05:12 +0900

    version 0.5

--- src/main/Command.java
+++ src/main/Command.java
@@ -1,37 +1,109 @@
 package main;
 
+import static main.Main.mailer;
+import static main.Main.msgList;
+
+import java.io.File;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
-import javax.security.auth.login.LoginException;
-
+import me.shivzee.callbacks.MessageFetchedCallback;
+import me.shivzee.util.Domains;
 import me.shivzee.util.JMailBuilder;
+import me.shivzee.util.Message;
+import me.shivzee.util.Response;
 
 public abstract class Command {
 	
-	public abstract void run(String[] args);
+	
+	protected abstract void run(String[] args) throws Exception;
 	public abstract String getUsage();
 	
-	//public boolean eq 
+	public static ArrayList<Command> cmdList = null;
+
 	
-	public static ArrayList<Command> getCmdList(){
-		return new ArrayList<Command>(Arrays.asList(
+	public void exec(String[] args) {
+		try {
+			run(args);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void invalidArgs() {
+		System.err.println("Invalid argument!");
+		System.err.flush();
+		System.out.println(getUsage());
+	}
+	
+	public void invalidArgs(String arg) {
+		System.err.println("Invalid argument : " + arg);
+		System.err.flush();
+		System.out.println(getUsage());
+	}
+	
+	public static ArrayList<Command> getCmdList() {
+		return cmdList;
+	}
+	
+	@SuppressWarnings("unlikely-arg-type")
+	public static Command getCmd(String cmd) {
+		for(Command c : cmdList) {
+			if(c.equals(cmd)) return c;
+		}
+		return new NULLCMD(cmd);
+	}
+	
+	public static void init(){
+		cmdList = new ArrayList<Command>(Arrays.asList(
 				
-				new Command() { // login
+				new Command() { // help
 
 					@Override
-					public void run(String[] args) {
+					public void run(String[] args) throws Exception {
+						
+						if(args.length == 0) {
+							for(Command c : cmdList) {
+								System.out.println(c.getUsage());
+								System.out.println();
+							}
+						} else if(args.length == 1 || !(getCmd(args[0]) instanceof NULLCMD)) {
+							System.out.println(getCmd(args[0]).getUsage());
+						} else {
+							invalidArgs();
+						}
+						
+					}
+
+					@Override
+					public String getUsage() {
+						return "help [command]\n"
+								+"\tGet usage of certain command.\n"
+								+"\tIf no argument is given, print help of all command.";
+					}
+					
+					@Override
+					public boolean equals(Object o) {
+						if(o == this || "help".equals(o)) return true;
+						else return false;
+					}
+					
+				},
+				new Command() { // login
+
+					//TODO : .autologin
+					@Override
+					public void run(String[] args) throws Exception {
 						if(args.length != 2) {
-							System.err.println("invalide argument!");
-							System.out.println(getUsage());
+							invalidArgs();
+							return;
 						}
-						
-						try {
-							Main.mailer = JMailBuilder.login(args[0] , args[1]);
-						} catch (LoginException e) {
-							e.printStackTrace();
-						}
-						
+						mailer = JMailBuilder.login(args[0] , args[1]);
+						mailer.init();
+						System.out.println("logged in to : " + mailer.getSelf().getEmail());
 					}
 
 					@Override
@@ -50,23 +122,28 @@ public abstract class Command {
 				new Command() { // gen
 
 					@Override
-					public void run(String[] args) {
-						if(args.length != 3 && args.length !=2) {
-							System.err.println("invalide argument!");
-							System.out.println(getUsage());
+					public void run(String[] args) throws Exception {
+						if (args.length != 1 && args.length != 2) {
+							invalidArgs();
+							return;
 						}
-						
-						if(args.length == 2) {
-							
+
+						if (args.length == 1) {
+							mailer = JMailBuilder.createDefault(args[0]);
+						} else {
+							String j = args[0] + "@" + Domains.getRandomDomain().getDomainName();
+							System.out.println(j);
+							mailer = JMailBuilder.createAndLogin(j, args[1]);
 						}
+						mailer.init();
+						System.out.println("logged in to : " + mailer.getSelf().getEmail());
 					}
 
 					@Override
 					public String getUsage() {
-						return "gen [id] --password <password>\r\n"
-								+ "\r\n"
-								+ "Generate new email combined with given id(optional) and random domain.\r\n"
-								+ "If id is not given, random id will be used.";
+						return "gen [id] <password>\n"
+								+ "\tGenerate new email combined with given id(optional) and random domain.\n"
+								+ "\tIf id is not given, random id will be used.";
 					}
 					
 					@Override
@@ -76,70 +153,283 @@ public abstract class Command {
 					}
 					
 				},
-				new Command() {
+				new Command() { // delaccount
 
 					@Override
-					public void run(String[] args) {
-						// TODO Auto-generated method stub
+					public void run(String[] args) throws Exception {
+						if (args.length != 0) {
+							invalidArgs();
+							return;
+						}
 						
+						mailer.delete(status->{ //TODO : asynchronous
+							   if(status) System.out.println("Account is Deleted");
+							});
 					}
 
 					@Override
 					public String getUsage() {
-						// TODO Auto-generated method stub
-						return null;
+						return "delaccount\n"
+								+ "\tDelete self account.";
 					}
 					
 					@Override
 					public boolean equals(Object o) {
-						if(o == this || "login".equals(o)) return true;
+						if(o == this || "delaccount".equals(o)) return true;
 						else return false;
 					}
 					
 				},
-				new Command() {
+				new Command() { // fetch
 
+					private boolean overwrite = false;
+					
 					@Override
-					public void run(String[] args) {
-						// TODO Auto-generated method stub
+					public void run(String[] args) throws Exception {
+						if (args.length >= 3) {
+							invalidArgs();
+							return;
+						}
 						
+						int limit = -1;
+						
+						for(String s : args) {
+							if("--overwrite".equals(s) || "-o".equals(s)) {
+								overwrite = true;
+							} else {
+								try {
+									limit = Integer.parseInt(s);
+								} catch (NumberFormatException e) {
+									invalidArgs(s);
+									return;
+								}
+							}
+						}
+						
+						MessageFetchedCallback callback = new MessageFetchedCallback() {
+							@Override
+							public void onMessagesFetched(List<Message> list) {
+								if(list.isEmpty()) {
+									System.out.println("No message to fetch!");
+									return;
+								}
+								consumMsgList(list);
+								System.out.println("Fetched Messages : ");
+								for (Message m : list) {
+									System.out.println("ID : " + m.getId());
+									System.out.println("Sent by : " + m.getSenderAddress());
+									System.out.println("Title : " + m.getSubject());
+									System.out.println();
+								}
+								System.out.println("Use listmsg command to show all fetched messages");
+							}
+
+							@Override
+							public void onError(Response response) {
+								System.err.println("Error while fetching messages! responseCode : " + response.getResponseCode());
+								System.err.println(response.getResponse());
+							}
+						};
+						
+						if (limit != -1) {
+							mailer.fetchMessages(Integer.parseInt(args[0]), callback);
+						} else {
+							mailer.fetchMessages(callback);
+						}
+
+					}
+					
+					private void consumMsgList(List<Message> list) {
+						if(overwrite) {
+							msgList = list;
+						} else {
+							list.stream().filter(m -> msgList.stream().map(Message::getId).allMatch(id -> !id.equals(m.getId()))).forEach(msgList::add);
+						}
 					}
 
 					@Override
 					public String getUsage() {
-						// TODO Auto-generated method stub
-						return null;
+						return "fetch [--overwrite|-o] [limit]\n" 
+								+ "\tFetch messages from server. To show messages, use 'listmsg' command.\n"
+								+ "\tIf --overwrite or -o option is given, fetched message before\n"
+								+ "\twill be overwrited to fresh fetched message\n"
+								+ "\tIf limit is given, up to <limit> messages will be fetched";
 					}
 					
 					@Override
 					public boolean equals(Object o) {
-						if(o == this || "login".equals(o)) return true;
+						if(o == this || "fetch".equals(o)) return true;
 						else return false;
 					}
 					
 				},
-				new Command() {
+				new Command() { // listmsg
 
 					@Override
-					public void run(String[] args) {
-						// TODO Auto-generated method stub
+					public void run(String[] args) throws Exception {
+						if (args.length != 0) {
+							invalidArgs();
+							return;
+						}
 						
+						for (Message m : msgList) {
+							System.out.println("ID : " + m.getId());
+							System.out.println("Sent by : " + m.getSenderAddress());
+							System.out.println("Title : " + m.getSubject());
+							System.out.println();
+						}
 					}
 
 					@Override
 					public String getUsage() {
-						// TODO Auto-generated method stub
-						return null;
+						return "listmsg\n"
+								+ "\tList all fetched message.";
 					}
 					
 					@Override
 					public boolean equals(Object o) {
-						if(o == this || "login".equals(o)) return true;
+						if(o == this || "listmsg".equals(o)) return true;
+						else return false;
+					}
+					
+				},
+				new Command() { // showmsg
+
+					@Override
+					public void run(String[] args) throws Exception {
+
+						if (args.length == 0) {
+							msgList.forEach(this::showMessage);
+						} else if (args.length == 1) {
+							for(Message m : msgList) {
+								if(args[0].equals(m.getId())) {
+									showMessage(m);
+									return;
+								}
+							}
+							invalidArgs(args[0] + " does not exist!");
+						} else {
+							invalidArgs();
+						}
+						
+						
+					}
+
+					private void showMessage(Message m) {
+						System.out.println("ID : " + m.getId());
+	                    System.out.println("Sent by : " + m.getSenderAddress() + " (" + m.getSenderName() + ")");
+	                    System.out.println("Title : " + m.getSubject());
+	                    if(m.hasAttachments()) {
+	                    	System.out.println("Attatchment(s) : " + m.getAttachments().stream().map(a -> a.getFilename() + "(" + a.getSize() + "KB)")
+	                    			.collect(Collectors.joining(", ")));
+	                    }
+	                    System.out.println();
+	                    System.out.println(m.getContent());
+	                    System.out.println();
+	                    System.out.println();
+					}
+					
+					@Override
+					public String getUsage() {
+						return "showmsg [id]\n"
+								+ "\tShow message that have given id.\n"
+								+ "\tIf id is not given, show ALL message.";
+					}
+					
+					@Override
+					public boolean equals(Object o) {
+						if(o == this || "showmsg".equals(o)) return true;
+						else return false;
+					}
+					
+				},
+				new Command() { // getattat
+
+					//TODO : asynchronous
+					private String path = "." + File.separator;
+
+					@Override
+					public void run(String[] args) throws Exception {
+						
+						String id;
+						
+						if(args.length == 1 && !args[0].equals("--path")) {
+							id = args[0];
+						} else if(args.length == 3 && args[1].equals("--path")) {
+							id = args[0];
+							path = args[2].replace("/", File.separator).replace("\\", File.separator);
+						} else {
+							invalidArgs();
+							return;
+						}
+						
+						for(Message m : msgList) {
+							if(id.equals(m.getId())) {
+								m.getAttachments().forEach(attat -> {
+									attat.save(getPath(), attat.getFilename(), status -> {
+										if (status) System.out.println(attat.getFilename() + " is Downloaded");
+									});
+								});
+								return;
+							}
+						}
+					}
+
+					private String getPath() { return path + File.separator; }
+					@Override
+					public String getUsage() {
+						return "getattat <id> [--path path]\n"
+								+"\tDownload attatchment of mail that have given id.\n"
+								+"\tIf path is given, the file(s) are stored in there.\n"
+								+"\tElse, files will be downloaded at working directory (" + Paths.get(".").toFile().getAbsolutePath() + ")";
+					}
+					
+					@Override
+					public boolean equals(Object o) {
+						if(o == this || "getattat".equals(o)) return true;
+						else return false;
+					}
+					
+				},
+				new Command() { // exit
+
+					@Override
+					public void run(String[] args) throws Exception { }
+
+					@Override
+					public String getUsage() {
+						return "exit\n"
+								+"\tKills the application.";
+					}
+					
+					@Override
+					public boolean equals(Object o) {
+						if(o == this || "exit".equals(o)) return true;
 						else return false;
 					}
 					
 				}
-				
 				));
 	}
+	
+	
+	static class NULLCMD extends Command {
+
+		private String str;
+
+		public NULLCMD(String cmd) { this.str = cmd; }
+		
+		@Override
+		protected void run(String[] args) throws Exception {
+			System.err.println(getUsage());
+			System.err.flush();
+		}
+
+		@Override
+		public String getUsage() {
+			return "Invalid command : " + str;
+		}
+		
+	};
+	
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,14 +1,43 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 import me.shivzee.JMailTM;
+import me.shivzee.util.Message;
 
 public class Main {
 
 	protected static JMailTM mailer = null;
+	protected static List<Message> msgList = new ArrayList<>(); 
+	
+	public final static String version = "v0.5";
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Command.init();
+		System.out.println("JTempMailClient " + version);
+		
+		Scanner sc = new Scanner(System.in);
+		String input = "";
+		
+		while(!input.equals("exit")) {
+			System.out.print(" $> ");
+			input = sc.nextLine();
+			int index = input.indexOf(" ");
+			if (index != -1 && index != input.length()) {
+				Command.getCmd(input.substring(0, index)).exec(input.substring(index + 1).split(" "));
+			} else {
+				Command.getCmd(input).exec(new String[] {});
+			}
+			System.err.flush();
+			System.out.flush();
+			System.out.println();
+		}
+		
+		sc.close();
+		
 	}
 
 }
